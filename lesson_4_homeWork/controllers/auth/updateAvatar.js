6cef1ed316c03b66a7b0c044118df4ed39bd2b31
VolodymyrVolosetskyi <57066225+Vovanchuk04071@users.noreply.github.com>
2023-06-30 09:21:48 +0300

    added cloudinary

--- lesson_4_homeWork/controllers/auth/updateAvatar.js
+++ lesson_4_homeWork/controllers/auth/updateAvatar.js
@@ -2,6 +2,15 @@ const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
 const { HttpCode } = require("../../helpers/constants");
+const cloudinary = require("cloudinary");
+
+const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET,
+});
 
 const avatarDir = path.join(__dirname, "../", "../", "public", "avatars");
 
@@ -13,16 +22,26 @@ const updateAvatar = async (req, res) => {
     const resultPath = path.join(avatarDir, imageName);
 
     await fs.rename(tempPath, resultPath);
+    const { secure_url } = await cloudinary.v2.uploader.upload(
+      resultPath,
+      { public_id: `avatars/${imageName}`, folder: "avatars" },
+      async (err, result) => {
+        if (err) throw err;
+        await fs.unlink(resultPath);
+        return result;
+      }
+    );
 
-    const avatarURL = path.join("public", "avatars", imageName);
-
-    await User.findByIdAndUpdate(_id, { avatarURL });
+    //
+    // const avatarURL = path.join("public", "avatars", imageName);
+    //
+    await User.findByIdAndUpdate(_id, { avatarURL: secure_url });
 
     res.json({
       status: "success",
       code: HttpCode.OK,
       data: {
-        avatarURL,
+        avatarURL: secure_url,
       },
     });
   } catch (error) {
